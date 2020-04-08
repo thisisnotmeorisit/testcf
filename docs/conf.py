4d431ec43e84ee73ca26e1f90a1c7bcc7fa0d32a
thisisnotmeorisit <63356461+thisisnotmeorisit@users.noreply.github.com>
2020-04-08 23:03:14 +0530

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -32,7 +32,7 @@ release = '1.0.0'
 # ones.
 extensions = [
     'sphinxcontrib.napoleon',
-    'sphinxcontrib-asyncio'
+    'sphinxcontrib.asyncio'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
